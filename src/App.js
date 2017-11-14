961b3516857f7e0f1f43091879e6c4f54edae07a
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-11-14 15:44:34 -0200

    Add jQuery for Ajax call
    - Using react lifecycle method 'componentDidMount()'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      lista: [
-        {
-          nome: 'test',
-          email: 'test@email.com',
-          senha: '123456'
-        }
-      ]
+      lista: []
     }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url:'http://localhost:8080/api/autores',
+      dataType: 'json',
+      success:function(resposta) {
+        this.setState = {lista:resposta};
+      }.bind(this)
+    })
   }
 
   render() {
@@ -23,11 +28,9 @@ class App extends Component {
         <a href="#menu" id="menuLink" className="menu-link">
           <span></span>
         </a>
-
         <div id="menu">
           <div className="pure-menu">
             <a className="pure-menu-heading" href="#">Company</a>
-
             <ul className="pure-menu-list">
               <li className="pure-menu-item">
                 <a href="#" className="pure-menu-link">Home</a>
@@ -41,7 +44,6 @@ class App extends Component {
             </ul>
           </div>
         </div>
-
         <div id="main">
           <div className="header">
             <h1>Cadastro de Autores</h1>
@@ -51,22 +53,21 @@ class App extends Component {
               <form className="pure-form pure-form-aligned">
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value=""/>
+                  <input id="nome" type="text" name="nome" value="" />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value=""/>
+                  <input id="email" type="email" name="email" value="" />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha"/>
+                  <input id="senha" type="password" name="senha" />
                 </div>
                 <div className="pure-control-group">
                   <label></label>
                   <button type="submit" className="pure-button pure-button-primary">Gravar</button>
                 </div>
               </form>
-
             </div>
             <div>
               <table className="pure-table">
@@ -77,23 +78,25 @@ class App extends Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {
-                    this.state.lista.map((autor, index) => {
+                  { this.state.lista.map((autor, index) => {
                       return (
-                        <tr key={index}>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
+                        <tr key={ index }>
+                          <td>
+                            { autor.nome }
+                          </td>
+                          <td>
+                            { autor.email }
+                          </td>
                         </tr>
                       )
-                    })
-                  }
+                    }) }
                 </tbody>
               </table>
             </div>
           </div>
         </div>
       </div>
-    );
+      );
   }
 }
 
